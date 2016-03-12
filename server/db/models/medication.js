c1921f52ccee61571c646980ceaf54a77c106c6f
Carolyn Zelenetz <carolyn.zelenetz@gmail.com>
2016-03-12 15:56:12 -0500

    medication model

--- server/db/models/medication.js
+++ server/db/models/medication.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const _ = require('lodash');
 
-let categories = ['Cardiac Arrest', 'Dysrhythmia', 'Critical Care', 'RSI', 'Seizure', 'Sedation', 'Pressor', 'Frequently Used', 'General'];
+let categories = ['cardiac arrest', 'dysrhythmia', 'critical care', 'rsi', 'seizure', 'sedation', 'pressor', 'frequently used', 'general'];
 
 const MedSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -11,7 +11,7 @@ const MedSchema = new mongoose.Schema({
   concentration: { type: Number, required: true }, //or should this be a default instead?
   dosage: { type: Number, required: true },
   doseUnit: { type: String, enum: ['mg', 'mEq', 'g', 'mcg', 'mL', 'unit'], required: true },
-  instructions: { type: String }  
+  instructions: { type: String }
 });
 
 MedSchema.set('toJSON', {
@@ -23,9 +23,10 @@ MedSchema
   .path('use')
   .validate(function(valueArr, respond) {
     let bool = valueArr.every((value) => {
-      return (categories.indexOf(value) >= -1);
+      return (categories.indexOf(value.toLowerCase()) >= -1);
     })
     respond(bool);
   }, 'Use must be a category');
+
 
 mongoose.model('Medication', MedSchema);
