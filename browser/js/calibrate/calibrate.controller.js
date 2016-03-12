c889bc8e9dd16a038cb18e3717bab23d7f8b32ba
Patrick Trasborg <patrick.trasborg@gmail.com>
2016-03-12 17:18:18 -0500

    added files to calibrate card color

---
+++ browser/js/calibrate/calibrate.controller.js
@@ -0,0 +1,19 @@
+'use strict';
+angular.module('InnovateNYP')
+.controller('CalibrateCtrl', function($scope, calibrationImage, Calibrate){
+
+  $scope.calibrationImage = calibrationImage;
+  var image = document.getElementById('img');
+
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+
+  image.onload = function(){
+    canvas.width = image.width;
+    canvas.height = image.height;
+  	
+    context.drawImage(image, 0, 0, image.width, image.height);
+    $scope.averageColor = Calibrate.calcAverageColor(context, image.width, image.height, 4);
+  };
+  
+});
---
+++ browser/js/calibrate/calibrate.service.js
@@ -0,0 +1,44 @@
+'use strict';
+
+angular.module('InnovateNYP')
+.factory('Calibrate', function($http){
+	var Calibrate = {};
+
+	Calibrate.fetchImage = function(){
+		return 'https://s3.amazonaws.com/rapgenius/1371963360_china-merchants-bank-unionpay-credit-card-visa-ctrip.jpg';
+	};
+
+	Calibrate.calcAverageColor = function(context, width, height, threshold){
+    tracking.Fast.THRESHOLD = threshold;
+
+    var imageData = context.getImageData(0, 0, width, height);
+    var gray = tracking.Image.grayscale(imageData.data, width, height);
+    var corners = tracking.Fast.findCorners(gray, width, height);
+
+    var pixelData = [];
+
+    for (var i = 0; i < corners.length; i += 2) {
+      var data = context.getImageData(corners[i],corners[i+1],1,1).data;
+      pixelData.push([data[0], data[1], data[2]]);
+      // context.fillStyle = '#f00';
+      // context.fillRect(corners[i], corners[i + 1], 3, 3);
+    }
+
+    return Calibrate.calculateAverage(pixelData);
+	};
+
+	Calibrate.calculateAverage = function(arr){
+    var averages = arr.reduce(function(acc, curr){
+      acc.r += curr[0];
+      acc.g += curr[1];
+      acc.b += curr[2];
+      return acc;
+    }, {r: 0, g: 0, b: 0});
+    averages.r = Math.round(averages.r / arr.length);
+    averages.g = Math.round(averages.g / arr.length);
+    averages.b = Math.round(averages.b / arr.length);
+    return averages;
+	};
+
+	return Calibrate;
+});
