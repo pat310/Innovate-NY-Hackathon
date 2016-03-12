1d651c9b7f2d812cf68cc7288f66a17cba09ac2d
mbthiery <mbthiery@gmail.com>
2016-03-12 18:00:03 -0500

    stateSetup

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -4,6 +4,19 @@ angular.module('InnovateNYP')
 .config(function ($stateProvider) {
   $stateProvider.state('home', {
     url: '/',
-    templateUrl: 'js/home/home.html'
+    templateUrl: 'js/home/home.html',
+    controller: 'HomeCtrl'
   });
+});
+
+app.controller('HomeCtrl', function ($scope, $state) {
+
+	$scope.submitMetric = function(metric, value){
+		// use conversion factory
+		$state.go("categories")
+	}	
+
+	$scope.getWeightMetric = function(){
+		return $scope.isUS ? "lbs" : "kgs"
+	}
 });
