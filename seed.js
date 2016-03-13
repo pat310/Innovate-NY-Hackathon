fdef6b48c7f20aaf54e8dafbe6fe8f43e91644b1
3mzel <mzelenetz@g.harvard.edu>
2016-03-12 20:26:21 -0500

    Added more Drugs and maxDose field

--- seed.js
+++ seed.js
@@ -15,6 +15,18 @@ in the database. If you are developing multiple applications with the
 fsg scaffolding, keep in mind that fsg always uses the same database
 name in the environment files.
 
+
+
+        {
+            name: '',
+            use: ['']
+            concentration: ,
+            dosage: ,
+            doseUnit: '',
+            instructions: ''
+            maxDose: ,
+        },
+
 */
 
 var mongoose = require('mongoose');
@@ -49,7 +61,7 @@ var seedUsers = function () {
 
 //Pat and Carolyn:
 //I will use the following categories (may add more as a think of them)
-// Cardiac Arrest, Dysrythmia, Critical Care [general], RSI, Seizure, Sedation, Pressor
+// Cardiac Arrest, Dysrythmia, Critical Care/ED [general], RSI, Seizure/Neuro, Sedation/Analgesia, Pressor
 var seedDrugs = function(){
     var drugs = [
         {
@@ -58,7 +70,8 @@ var seedDrugs = function(){
             concentration: 1,
             dosage: 0.1,
             doseUnit: 'mg',
-            instructions: 'Comes in 3mg/mL. Dilute to 1mg/mL [2ml into 4ml NS]'
+            instructions: 'Comes in 3mg/mL. Dilute to 1mg/mL [2ml into 4ml NS]. Double the amount for second dose.'
+            maxDose: 6,
         },
         {
             name: 'Amiodarone',
@@ -67,6 +80,7 @@ var seedDrugs = function(){
             dosage: 5,
             doseUnit: 'mg',
             instructions: '[Mix 150mg (3mL) + 47mL D5W. Administer over 10 min.]'
+            maxDose: 150,
         },
         {
             name: 'Atropine',
@@ -75,23 +89,304 @@ var seedDrugs = function(){
             dosage: 0.02,
             doseUnit: 'mg',
             instructions: ''
+            maxDose: 0.5,
         },
         {
             name: 'Sodium Bicarbonate',
-            use: ['Cardiac Arrest', 'Critical Care'],
+            use: ['Cardiac Arrest', 'Critical Care/ED'],
             concentration: 1,
             dosage: 1,
             doseUnit: 'mEq',
             instructions: ''
+            maxDose: 50,
         },
         {
+            name: 'Calcium Chloride',
+            use: ['Cardiac Arrest', 'Critical Care/ED', 'Dysrhythmia'],
+            concentration: 100,
+            dosage: 20,
+            doseUnit: 'mg',
+            instructions: ''
+            maxDose: 1000,
+        },
+        {
+            name: 'Dextrose 50%',
+            use: ['Critical Care/ED', 'Seizure/Neuro'],
+            concentration: 0.5,
+            dosage: 0.5,
+            doseUnit: 'g',
+            instructions: ''
+            maxDose: 25,
+        },
+        {
+            name: 'Epinephrine 1:10,000',
+            use: ['Cardiac Arrest', 'Critical Care/ED'],
+            concentration: 0.1,
+            dosage: 0.01,
+            doseUnit: 'mg',
+            instructions: ''
+            maxDose: 1,
+        },
+        {
+            name: 'Epinephrine 1:1,000',
+            use: ['Cardiac Arrest', 'Critical Care/ED'],
+            concentration: 1,
+            dosage: 0.01,
+            doseUnit: 'mg',
+            instructions: 'IM/SC'
+            maxDose: 0.3,
+        },
+        {
+            name: 'Flumazenil',
+            use: ['Critical Care/ED'],
+            concentration: 100,
+            dosage: 5,
+            doseUnit: 'mcg',
+            instructions: ''
+            maxDose: ,
+        },
+        {
+            name: 'Hypertonic Saline',
+            use: ['Seizure/Neuro'],
+            concentration: ,
+            dosage: 0.5,
+            doseUnit: 'ml',
+            instructions: ''
+            maxDose: 30,
+        },
+
+        {
+            name: 'Lorazepam',
+            use: ['Seizure/Neuro', 'Sedation/Analgesia'],
+            concentration: 2,
+            dosage: 0.05,
+            doseUnit: 'mg',
+            instructions: ''
+            maxDose: 4,
+        },
+
+        {
+            name: 'Lidocaine 2%',
+            use: ['Cardiac Arrest', 'Dysrhythmia'],
+            concentration: 20,
+            dosage: 1,
+            doseUnit: 'mg',
+            instructions: ''
+            maxDose: ,
+        },
+
+        {
+            name: 'Magnesium Sulfate',
+            use: ['Dysrhythmia', 'Critical Care/ED'],
+            concentration: 500,
+            dosage: 25,
+            doseUnit: 'mg',
+            instructions: ''
+            maxDose: 20000,
+        },
+        {
+            name: 'Mannitol',
+            use: ['Seizure/Neuro'],
+            concentration: 0.25,
+            dosage: 0.25,
+            doseUnit: 'g',
+            instructions: ''
+            maxDose: 12.5,
+        },
+        {
+            name: 'Naloxone',
+            use: ['Critical Care/ED'],
+            concentration: 0.4,
+            dosage: 0.01,
+            doseUnit: 'mg',
+            instructions: ''
+            maxDose: 2,
+        },
+        {
+            name: 'Procainamide',
+            use: ['Dysrhythmia'],
+            concentration: 100,
+            dosage: 15,
+            doseUnit: 'mg',
+            instructions: 'Administer over 30-60min until therapeutic/toxic effect'
+            maxDose: 1000,
+        },
+        {
+            name: 'Phenylephrine',
+            use: ['Pressor'],
+            concentration: ,
+            dosage: ,
+            doseUnit: '',
+            instructions: 'Comes 10mg/ml. Mix 0.1ml in 9.9ml NS'
+            maxDose: ,
+        },
+        {
+            name: 'Vasopressin',
+            use: ['Cardiac Arrest', 'Pressor'],
+            concentration: 20,
+            dosage: 0.5,
+            doseUnit: 'unit',
+            instructions: ''
+            maxDose: 40,
+        },
+        {
+            name: 'Etomidate',
+            use: ['RSI'],
+            concentration: 2,
+            dosage: 0.3,
+            doseUnit: 'mg',
+            instructions: ''
+            maxDose: ,
+        },
+        {
+            name: 'Fentanyl',
+            use: ['RSI', 'Sedation/Analgesia']
+            concentration: 50,
+            dosage: 1,
+            doseUnit: 'mcg',
+            instructions: ''
+            maxDose: ,
+        },
+        {
+            name: 'Ketamine',
+            use: ['RSI', 'Sedation/Analgesia']
+            concentration: 10,
+            dosage: mg,
+            doseUnit: '2',
+            instructions: ''
+            maxDose: ,
+        },
+        {
+            name: 'Midazolam',
+            use: ['RSI', 'Sedation/Analgesia']
+            concentration: 1,
+            dosage: 0.1,
+            doseUnit: 'mg',
+            instructions: ''
+            maxDose: 4,
+        },
+        {
+            name: 'Propofol',
+            use: ['RSI', 'Sedation/Analgesia']
+            concentration: 10,
+            dosage: 2,
+            doseUnit: 'mg',
+            instructions: 'Push Dose'
+            maxDose: 40,
+        },
+        {
+            name: 'Rocuronium',
+            use: ['RSI']
+            concentration: 10,
+            dosage: 1,
+            doseUnit: 'mg',
+            instructions: ''
+            maxDose: ,
+        },
+        {
+            name: 'Succinylcholine',
+            use: ['RSI']
+            concentration: 20,
+            dosage: 1,
+            doseUnit: 'mg',
+            instructions: ''
+            maxDose: ,
+        },
+
+        {
+            name: 'Vecuronium',
+            use: ['RSI']
+            concentration: 1,
+            dosage: 0.1,
+            doseUnit: 'mg',
+            instructions: ''
+            maxDose: ,
+        },
+
+        {
+            name: 'Dobutamine 1:1',
+            use: ['Pressor']
+            concentration: 1000,
+            dosage: 2,
+            doseUnit: 'mcg/kg/min',
+            instructions: '250mg/250ml'
+            maxDose: ,
+        },
+
+        {
+            name: 'Defibrillation',
+            use: ['Cardiac Arrest']
+            concentration: 1,
+            dosage: 2,
+            doseUnit: 'J',
+            instructions: ''
+            maxDose: 300,
+        },
+        {
+            name: 'Cardioversion',
+            use: ['Dysrhythmia']
+            concentration: 1,
+            dosage: 0.5,
+            doseUnit: 'J',
+            instructions: ''
+            maxDose: ,
+        },
+        {
+            name: 'Dopamine',
+            use: ['Pressor']
+            concentration: 800,
+            dosage: 2,
+            doseUnit: 'mcg/kg/min',
+            instructions: '200mg/250ml'
+            maxDose: ,
+        },
+
+        {
+            name: 'Epinephrine Drip',
+            use: ['Pressor']
+            concentration: 10,
+            dosage: 0.05,
+            doseUnit: 'mcg/kg/min',
+            instructions: '0.5mg/50ml'
+            maxDose: ,
+        },
+        {
+            name: 'Norepinephrine Drip',
+            use: ['Pressor']
+            concentration: 10,
+            dosage: 0.05,
+            doseUnit: 'mcg/kg/min',
+            instructions: '0.5mg/50ml'
+            maxDose: ,
+        },
+
+        {
             name: '',
-            use: [''],
+            use: ['']
             concentration: ,
             dosage: ,
             doseUnit: '',
             instructions: ''
+            maxDose: ,
         },
+
+        {
+            name: '',
+            use: ['']
+            concentration: ,
+            dosage: ,
+            doseUnit: '',
+            instructions: ''
+            maxDose: ,
+        },
+
+
+
+
+
+
+
+
 
     ];
 
