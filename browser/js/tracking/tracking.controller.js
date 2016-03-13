a3085db902d422cbae9d14ef92c384cf94ef582b
Patrick Trasborg <patrick.trasborg@gmail.com>
2016-03-12 21:56:15 -0500

    baby height measured

--- browser/js/tracking/tracking.controller.js
+++ browser/js/tracking/tracking.controller.js
@@ -1,11 +1,21 @@
 'use strict';
 angular.module('InnovateNYP')
 .controller('TrackingCtrl', function($scope, Tracking){
-	$scope.imageSource = "../../assets/babyrotate.jpg";
-  var img = document.getElementById('img');
-	var div = document.querySelector('.image-container');
-  img.onload = function(){
-  	Tracking.determineBody(img, div);
-  	Tracking.determineMarker(img, div);
-  };
+	$scope.loadPicture = function(){	
+		$scope.showLoader = true;
+		$scope.imageSource = "../../assets/babyrotate.jpg";
+	  var img = document.getElementById('img');
+		var div = document.querySelector('.image-container');
+
+
+	  img.onload = function(){
+			Tracking.determinePatientHeight(img, div)
+			.then(function(height){
+				$scope.showLoader = false;
+	  		$scope.$digest();
+	  		console.log('height', height);
+			});
+	  };
+	};
+	
 });
