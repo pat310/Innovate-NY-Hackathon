210083e277c2cc2b82dc181defcf60c62b39431c
Patrick Trasborg <patrick.trasborg@gmail.com>
2016-03-12 20:00:41 -0500

    credit card color working

--- browser/js/calibrate/calibrate.controller.js
+++ browser/js/calibrate/calibrate.controller.js
@@ -1,20 +1,61 @@
 'use strict';
 angular.module('InnovateNYP')
-.controller('CalibrateCtrl', function($scope, calibrationImage, Calibrate){
+.controller('CalibrateCtrl', function($scope, Calibrate){
 
-  $scope.source = '../../assets/creditcard.png';
-  var image = document.getElementById('img');
-
+  $scope.selectPicture = true;
+  var image = new Image();
   var canvas = document.getElementById('canvas');
   var context = canvas.getContext('2d');
 
-  image.onload = function(){
-    canvas.width = image.width;
-    canvas.height = image.height;
-  	
-    context.drawImage(image, 0, 0, image.width, image.height);
+  $scope.loadPicture = function(){
 
-    $scope.averageColor = Calibrate.calcAverageColor(context, image.width, image.height, 4);
+    $scope.showLoader = true;
+
+    image.src = '../../assets/creditcard.png';
+
+
+    image.onload = function(){
+      $scope.selectPicture = false;
+      $scope.showLoader = false;
+      $scope.$digest();
+
+      canvas.width = image.width;
+      canvas.height = image.height;
+      
+      context.drawImage(image, 0, 0, image.width, image.height);
+
+
+    };
+  };
+
+  $scope.colorSelected = function(evt){
+    var data = context.getImageData(evt.offsetX, evt.offsetY,1,1).data;
+    $scope.backgroundColor = {'background-color':`rgb(${data[0]}, ${data[1]}, ${data[2]})`};
+  };
+
+  $scope.analyzeAutomatically = function(){  
+    $scope.showLoader = true;
+
+    var image = new Image();
+    image.src = '../../assets/creditcard.png';
+
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+
+    image.onload = function(){
+      canvas.width = image.width;
+      canvas.height = image.height;
+    	
+      context.drawImage(image, 0, 0, image.width, image.height);
+
+      Calibrate.calcAverageColor(context, image.width, image.height, 4)
+      .then(function(averages){
+        $scope.showLoader = false;
+        $scope.$digest();
+        console.log('averages', averages);
+        $scope.backgroundColor = {'background-color':`rgb(${averages.r}, ${averages.g}, ${averages.b})`};
+      });
+    };
   };
   
 });
--- browser/js/calibrate/calibrate.js
+++ browser/js/calibrate/calibrate.js
@@ -5,11 +5,6 @@ angular.module('InnovateNYP')
 	$stateProvider.state('calibrate', {
 		url: '/calibrate',
 		templateUrl: 'js/calibrate/calibrate.html',
-		controller: 'CalibrateCtrl',
-		resolve:{
-			calibrationImage: function(Calibrate){
-				return Calibrate.fetchImage;
-			}
-		}
+		controller: 'CalibrateCtrl'
 	});
 });
